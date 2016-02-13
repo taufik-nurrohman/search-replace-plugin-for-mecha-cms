2955bfe11d39cd5d6a25c744a0e15c5043cdfa09
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-02-13 16:55:42 +0700

    Updates to Support MTE and HTE 1.5.x

--- __search-replace/assets/sword/button.js
+++ __search-replace/assets/sword/button.js
@@ -4,7 +4,7 @@
         grip = editor.grip,
         mte = base.languages.MTE,
         speak = mte.plugin_search_replace,
-        name = 'search plugin-search-replace';
+        name = 'binoculars plugin-search-replace';
     editor.button(name, {
         title: speak[0],
         click: function() {
@@ -26,7 +26,7 @@
                         pattern = pattern.replace(escape, '\\$1');
                     }
                     if (input[0].value === "") {
-                        return editor.close(true), false;
+                        return editor.exit(true), false;
                     }
                     var match = new RegExp(pattern, flag);
                     if (!input[2].checked) {
@@ -34,17 +34,14 @@
                         area.value = area.value.replace(match, input[1].value);
                         str = str !== null && str[0] ? str[0].replace(match, input[1].value) : "";
                         index = area.value.indexOf(str);
-                        grip.select(index, index + str.length);
-                        editor.close(true);
-                        grip.updateHistory();
+                        grip.select(index, index + str.length, true);
                     } else {
                         grip.replace(match, input[1].value);
                     }
-                    editor.close(true);
-                    return false;
+                    return editor.close(true), false;
                 }
-                ok.innerHTML = mte.buttons.ok;
-                cancel.innerHTML = mte.buttons.cancel;
+                ok.innerHTML = mte.actions.ok;
+                cancel.innerHTML = mte.actions.cancel;
                 var search = d.createElement('input'),
                     replace = d.createElement('input'),
                     option = d.createElement('label');
@@ -57,42 +54,42 @@
                 option.className = 'input';
                 option.innerHTML = '<input type="checkbox"' + (s.value.length > 0 ? ' checked' : "") + '> ' + speak[3];
                 editor.event("keydown", search, function(e) {
-                    var _this = this;
-                    if (e.keyCode === 13) {
-                        replace.value = search.value;
-                        return process();
-                    }
-                    if (e.keyCode === 38) return false;
-                    if (e.keyCode === 40) return replace.focus(), false;
-                    if (e.keyCode === 27 || e.ctrlKey && e.keyCode === 70) return editor.close(true), false;
+                    var _this = this, k = grip.key(e);
+                    if (k === 'enter') return replace.value = search.value, process();
+                    if (k === 'arrowup') return false;
+                    if (k === 'arrowdown') return replace.focus(), false;
+                    if (k === 'escape' || e.ctrlKey && k === 'f') return editor.exit(true), false;
                     w.setTimeout(function() {
                         var _regex = _this.value.match(/^\/.*?\/[gimuy]*$/);
                         _this.style.backgroundColor = _regex ? '#DBFFD6' : "";
                         _this.style.color = _regex ? '#000' : "";
-                    }, 1);
+                    }, .1);
                 });
                 editor.event("keydown", replace, function(e) {
-                    if (e.keyCode === 13) return process();
-                    if (e.keyCode === 27) return editor.close(true), false;
-                    if (e.keyCode === 38) return search.focus(), false;
-                    if (e.keyCode === 40) return ok.focus(), false;
+                    var k = grip.key(e);
+                    if (k === 'enter') return process();
+                    if (k === 'escape') return editor.exit(true), false;
+                    if (k === 'arrowup') return search.focus(), false;
+                    if (k === 'arrowdown') return ok.focus(), false;
                 });
                 editor.event("click", ok, process);
                 editor.event("click", cancel, function() {
-                    return editor.close(true), false;
+                    return editor.exit(true), false;
                 });
                 editor.event("keydown", ok, function(e) {
-                    if (e.keyCode === 13) return process();
-                    if (e.keyCode === 27) return editor.close(true), false;
-                    if (e.keyCode === 38) return replace.focus(), false;
-                    if (e.keyCode === 39 || e.keyCode === 40) return cancel.focus(), false;
+                    var k = grip.key(e);
+                    if (k === 'enter') return process();
+                    if (k === 'escape') return editor.exit(true), false;
+                    if (k === 'arrowup') return replace.focus(), false;
+                    if (k.match(/^arrow(right|down)$/)) return cancel.focus(), false;
                 });
                 editor.event("keydown", cancel, function(e) {
-                    if (e.keyCode === 13 || e.keyCode === 27) return editor.close(true), false;
-                    if (e.keyCode === 37 || e.keyCode === 38) return ok.focus(), false;
-                    if (e.keyCode === 40) return false;
+                    var k = grip.key(e);
+                    if (k.match(/^enter|escape$/)) return editor.exit(true), false;
+                    if (k.match(/^arrow(left|up)$/)) return ok.focus(), false;
+                    if (k === 'arrowdown') return false;
                 });
-                header.innerHTML = speak[0];
+                header.innerHTML = speak[0][0];
                 content.appendChild(search);
                 content.appendChild(replace);
                 content.appendChild(option);
@@ -100,11 +97,12 @@
                 footer.appendChild(cancel);
                 w.setTimeout(function() {
                     search.focus();
-                }, 10);
+                }, .2);
             });
         }
     });
-    editor.shortcut('CTRL+70', function() {
+    // `Ctrl + F` to "search/replace"
+    editor.shortcut('ctrl+f', function() {
         return base.composer.grip.config.buttons[name].click(), false;
     });
 })(window, document, DASHBOARD);
--- __search-replace/launch.php
+++ __search-replace/launch.php
@@ -1,9 +1,7 @@
 <?php
 
-Config::merge('DASHBOARD.languages.MTE', array(
-    'plugin_search_replace' => (array) $speak->plugin_search_replace
-));
+Config::merge('DASHBOARD.languages.MTE.plugin_search_replace', $speak->plugin_search_replace);
 
 Weapon::add('SHIPMENT_REGION_BOTTOM', function() {
-    echo Asset::javascript(__DIR__ . DS . 'assets' . DS . 'sword' . DS . 'button.js');
+    echo Asset::javascript(__DIR__ . DS . 'assets' . DS . 'sword' . DS . 'button.js', "", 'sword/editor.button.' . ltrim(File::B(__DIR__), '_') . '.min.js');
 }, 20);
